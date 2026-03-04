96313779634ccde44e2f0cda5728e416b5bd5eca
Lucifer-Dennica <denis22142qwe@gmail.com>
2026-03-04 21:50:57 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -313,4 +313,4 @@ async def delete_range_end(message: Message, state: FSMContext, bot):
     await message.answer(f"Все доступные слоты с {start_str} по {time_str} на {date_str} удалены.")
     await state.clear()
 
-Fix edit_text exception
+#Fix edit_text exception
